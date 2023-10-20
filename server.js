bcb12c2ce50c138ada44bb6118ebd4ed7bdcbf89
bbeck9123 <byron.ptgo@gmail.com>
2023-10-19 18:03:54 -0700

    JSON - savage

--- server.js
+++ server.js
@@ -2,18 +2,34 @@ const express = require('express')
 const app = express()
 const PORT = 8000
 
-const savage = {
+const rappers = {
+    '21 Savage': {
     'age': 29,
     'birthName': 'Sheyaa',
     'birthLocation': 'London, Englance'
+    },
+
+    'Chance tha Rapper': {
+    'age': 29,
+    'birthName': 'Doushe ',
+    'birthLocation': 'Philly'
+    },
+
+    '50 cent': {
+    'age': 29,
+    'birthName': 'Curtis Jackson',
+    'birthLocation': 'Bronx, New York'
+    }
+    
 }
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
-app.get('/api', (req, res) => {
-    res.json(savage)
+app.get('/api/:name', (req, res) => {
+    req.params.name
+    res.json(rappers)
 })
 
 
