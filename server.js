d2972efbe6f73dbc20864bc933383e775761c987
bbeck9123 <byron.ptgo@gmail.com>
2023-10-19 17:29:30 -0700

    JSON - savage

--- server.js
+++ server.js
@@ -2,9 +2,20 @@ const express = require('express')
 const app = express()
 const PORT = 8000
 
+const savage = {
+    'age': 29,
+    'birthName': 'Sheyaa',
+    'birthLocation': 'London, Englance'
+}
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
+
+app.get('/api', (req, res) => {
+    res.json(savage)
+})
+
 
 app.listen(PORT, () => {
     console.log(`You are rocking on port ${PORT}`)
